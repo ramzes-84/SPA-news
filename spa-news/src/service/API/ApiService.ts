e3b68af54291277e2774c854721c48f88c306fa4
Roman Shevyakov <roman@mgstr.ru>
2023-09-23 16:10:24 +0600

    feat: add ability to get news for main page

--- spa-news/src/service/API/ApiService.ts
+++ spa-news/src/service/API/ApiService.ts
@@ -1,4 +1,15 @@
 const { API_KEY } = process.env;
+const CONTENT_ENDPOINT = 'https://content.guardianapis.com/search';
 
-export default class ApiService {
+export class ApiService {
+  public async getNews () {
+    const url = `${CONTENT_ENDPOINT}?api-key=${API_KEY}&show-fields=thumbnail`;
+    const response = await fetch(url);
+    if(response.status === 200) {
+      const resFromJSON = await response.json();
+      console.log(resFromJSON.response.results);
+      return resFromJSON.response.results;
+    }
+    throw new Error('There is something wrong with server!')
+  }
 }
