0d09ae1f4f672603c72a547c68a1e5b8fcb275a8
Roman Shevyakov <roman@mgstr.ru>
2023-09-23 16:08:35 +0600

    feat: add types

--- spa-news/next.config.js
+++ spa-news/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.guim.co.uk',
+      },
+    ],
+  },
+
+}
 
 module.exports = nextConfig
---
+++ spa-news/src/app/types.ts
@@ -0,0 +1,14 @@
+export type ArticleInCatalog = {
+  fields: {thumbnail: string};
+  id: string;
+  type: string;
+  sectionId: string;
+  sectionName: string;
+  webPublicationDate: string;
+  webTitle: string;
+  webUrl: string;
+  apiUrl: string;
+  isHosted: boolean;
+  pillarId: string;
+  pillarName: string;
+}
