23efe53ddb6549bf44ccca7bb56d69cfbe41c740
Roman Shevyakov <roman@mgstr.ru>
2023-09-24 09:42:46 +0600

    feat: complete Article Page

--- spa-news/src/app/article/[...articleID]/page.tsx
+++ spa-news/src/app/article/[...articleID]/page.tsx
@@ -1,16 +1,20 @@
 import { ArticleInCatalog } from "@/app/types";
 import { ApiService } from "@/service/API/ApiService";
 import Image from 'next/image';
+import Link from "next/link";
 
 export default async function Page({ params }: { params: { articleID: string[] } }) {
   const id = params.articleID.join('/');
   const apiService = new ApiService();
   const article: ArticleInCatalog = await apiService.getCurrentArticle(id);
+  const articleDate = new Date(article.webPublicationDate);
 
   return (
     <article className="flex flex-col	items-center p-3	">
-      <h1 className="underline-offset-1	m-2 text-center	text-sky-900 font-extrabold	text-xl		">{article.webTitle}</h1>
+      <h1 className="underline-offset-1	m-2 text-center	text-sky-900 font-extrabold	text-xl">{article.webTitle}</h1>
+      <p>Date: {`${articleDate.getFullYear()}-${articleDate.getMonth()+1}-${articleDate.getDate()}`}</p>
       <Image src={article.fields.thumbnail} width={420} height={250} alt="" />
+      <Link href={article.webUrl}><button className="p-2 m-2 rounded-2xl bg-slate-500 text-white">Read on Guardian site</button></Link>
       <div dangerouslySetInnerHTML={{ __html: article.fields.body }} />
     </article>
   )
