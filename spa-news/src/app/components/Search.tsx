57ce7087ab7a0d591941a6616b3412cc7508422d
Roman Shevyakov <roman@mgstr.ru>
2023-09-25 14:21:10 +0600

    feat: add autoscroll

--- spa-news/src/app/components/Search.tsx
+++ spa-news/src/app/components/Search.tsx
@@ -1,6 +1,6 @@
 import { FormEvent } from "react"
 import { fetchNews } from "./server-actions";
-import { ArticleInCatalog, RequestParams, Sort } from "../types";
+import { ArticleInCatalog, Sort } from "../types";
 import { params } from "../page";
 
 export function Search(
@@ -16,7 +16,7 @@ export function Search(
     const formData = new FormData(event.currentTarget);
     const formJSON = Object.fromEntries(formData.entries());
     params.limit = formJSON.limit as unknown as number;
-    params.sort = formJSON.sort as string;
+    params.sort = formJSON.sort as Sort;
     if(formJSON.keyword) params.keyword = formJSON.keyword as string;
     const foundNews = await fetchNews(params);
     newsCallback(foundNews);
--- spa-news/src/app/components/server-actions.ts
+++ spa-news/src/app/components/server-actions.ts
@@ -9,3 +9,18 @@ export async function fetchNews(params: RequestParams) {
   const newsArr: ArticleInCatalog[] = await apiService.getNews(limit, sort, page, keyword);
   return newsArr;
 }
+
+export async function fetchNextPageNews(params: RequestParams) {
+  params.page += 1;
+  const {keyword, limit, sort, page} = params;
+  const apiService = new ApiService();
+  const newsArr: ArticleInCatalog[] = await apiService.getNews(limit, sort, page, keyword);
+  return newsArr;
+}
+
+export async function autoFetchMoreNews(params: RequestParams, oldNews: ArticleInCatalog[]) {
+  params.page += 1;
+  const nextPageNews = await fetchNextPageNews(params);
+  const news = [...oldNews, ...nextPageNews];
+  return news;
+}
