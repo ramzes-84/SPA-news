72a86a29ff681817bdbe07a6d47d5024e2bef4a3
“Roman <roman@mgstr.ru>
2023-10-08 15:48:21 +0600

    refactor: change Search component

--- spa-news/src/app/components/Search.tsx
+++ spa-news/src/app/components/Search.tsx
@@ -1,37 +1,41 @@
-import { FormEvent, useContext } from 'react';
-import { IContext, RequestParams, Sort } from '../types';
+import { ChangeEvent, useContext } from 'react';
+import { IContext } from '../types';
 import { Context } from './NewsCatalog';
 
 export function Search() {
-  const { setConfig } = useContext(Context) as unknown as IContext;
+  const { config, setConfig } = useContext(Context) as unknown as IContext;
+  let timerID: string | number | NodeJS.Timeout | undefined;
 
-  async function handleSearch(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const formJSON = Object.fromEntries(formData.entries());
-    const newConfig: RequestParams = {
-      limit: formJSON.limit as unknown as number,
-      sort: formJSON.sort as Sort,
-      keyword: formJSON.keyword as string,
-      page: 1,
-    };
-    setConfig(newConfig);
+  function onChange(e: ChangeEvent<HTMLSelectElement | HTMLInputElement>) {
+    e.preventDefault;
+    if (e.target instanceof HTMLInputElement && e.target.value.length < 3) {
+      return;
+    }
+    if (timerID) clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      setConfig({ ...config, [e.target.name]: e.target.value });
+    }, 1000);
   }
 
   return (
     <section className="rounded-2xl bg-slate-500 text-white m-2 p-2">
-      <form className="flex justify-center gap-2" name="myInput" onSubmit={handleSearch}>
-        <input className="text-black" type="text" name="keyword" />
-        <select className="text-black" name="limit">
+      <form className="flex justify-center gap-2" name="myInput">
+        <input
+          placeholder="Enter 3 or more letters..."
+          className="text-black"
+          type="text"
+          name="keyword"
+          onChange={(e) => onChange(e)}
+        />
+        <select className="text-black" name="limit" onChange={(e) => onChange(e)} defaultValue={config.limit}>
           <option value="10">10 items per page</option>
           <option value="20">20 items per page</option>
         </select>
-        <select className="text-black" name="sort">
+        <select className="text-black" name="sort" onChange={(e) => onChange(e)} defaultValue={config.sort}>
           <option value="newest">Show newest first</option>
           <option value="oldest">Show oldest first</option>
           <option value="relevance">Sort by relevance</option>
         </select>
-        <button type="submit">Get news!</button>
       </form>
     </section>
   );
